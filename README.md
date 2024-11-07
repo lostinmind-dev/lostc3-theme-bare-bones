b26aa6af83674297b25331fe06322a737bdfa64d
Nikita <61884745+dakln@users.noreply.github.com>
2024-11-08 02:29:35 +0500

    v1.2.5

---
+++ README.md
@@ -0,0 +1,10 @@
+# [Theme] Addon bare-bones
+
+There is a Lost bare-bones project for 'theme' addon type.
+
+<!-- ## Installation
+Use `lost create --theme` -->
+
+## Development
+- Use `deno task build` OR `lost build` to build addon.
+- Use `deno task serve` OR `lost serve` to build AND start web development server for testing addon.
---
+++ lost.config.ts
@@ -0,0 +1,15 @@
+import type { LostConfig } from "jsr:@lost-c3/lib@1.2.5";
+
+const Config: LostConfig<'theme'> = {
+    Type: 'theme',
+
+    AddonId: 'Lost_MyTheme',
+    AddonName: 'Lost Theme',
+    AddonDescription: 'Amazing theme made with Lost.',
+    Version: '1.0.0.0',
+    Author: 'lostinmind.',
+    WebsiteURL: `https://addon.com`,
+    DocsURL: `https://docs.addon.com`,
+};
+
+export default Config;
